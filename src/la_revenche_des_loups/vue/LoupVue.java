9bde10b4fdfd961a3640690dce836d1beb1cf26e
cliu <cliu@iut.univ-paris8.fr>
2020-05-18 12:15:25 +0200

    modification Loup

---
+++ src/la_revenche_des_loups/vue/LoupVue.java
@@ -0,0 +1,35 @@
+package la_revenche_des_loups.vue;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.TilePane;
+import la_revenche_des_loups.modele.Loup;
+import la_revenche_des_loups.modele.Terrain;
+
+public class LoupVue {
+	private TilePane panneauJeu;
+	private Terrain terrain;
+	
+	public LoupVue(TilePane panneau, Terrain terrain) {
+		this.panneauJeu = panneau;
+		this.terrain = terrain;
+	}
+	
+	public void afficherLoupVue(Loup loup) {
+		Image loupVue = null;
+        try {
+        	loupVue = new Image(new FileInputStream("src/la_revenche_des_loups/modele/Loup_Gris.png"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+		ImageView imageview = new ImageView(loupVue);
+		imageview.setFitWidth(12);
+		imageview.setFitHeight(12);
+		imageview.setTranslateX(loup.getX()*12);
+		imageview.setTranslateY(loup.getY()*12);
+		this.panneauJeu.getChildren().add(imageview);
+	}
+}
