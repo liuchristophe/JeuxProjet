9500b186e6f7fb5efc412ffaf6cec3c7aab9c51a
myahia93 <mohcine.93.yahia@gmail.com>
2020-05-20 11:20:47 +0200

    test

--- src/la_revenche_des_loups/controleur/Controleur.java
+++ src/la_revenche_des_loups/controleur/Controleur.java
@@ -14,11 +14,8 @@ import la_revenche_des_loups.modele.Jeu;
 import la_revenche_des_loups.modele.Loup;
 import la_revenche_des_loups.modele.Maison;
 import la_revenche_des_loups.modele.Terrain;
-<<<<<<< HEAD
 import la_revenche_des_loups.vue.GameLoop;
-=======
 import la_revenche_des_loups.modele.Tour;
->>>>>>> c1a4df185e0fc4296f5b48d3a3383acd606d6c94
 import la_revenche_des_loups.vue.LoupVue;
 import la_revenche_des_loups.vue.MaisonVue;
 import la_revenche_des_loups.vue.TerrainVue;
@@ -28,26 +25,19 @@ public class Controleur implements Initializable{
 
 	private Terrain terrain;
 	private Jeu jeu;
-<<<<<<< HEAD
 	private Loup loup;
 	private Maison maison;
 	private TerrainVue terrainVue;
 	private LoupVue loupVue;
 	private MaisonVue maisonVue;
 	private GameLoop gameloop;
-=======
-	private TerrainVue terrainVue;
-	private LoupVue loupVue;
-	private Loup loup;
 	private TourVue tourVue;
->>>>>>> c1a4df185e0fc4296f5b48d3a3383acd606d6c94
     @FXML private TilePane tilePane;
     @FXML private Pane tableDeJeu;
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		this.terrain=new Terrain();
-<<<<<<< HEAD
 		this.jeu = new Jeu(terrain);
 		this.maison = new Maison(jeu); //testMaisonVue
 		this.loup = new Loup(jeu);
@@ -58,27 +48,22 @@ public class Controleur implements Initializable{
 		this.maisonVue.creerMaisonVue(maison); //testMaisonVue
 		this.loupVue = new LoupVue(this.tableDeJeu, jeu.getTerrain());
 		this.gameloop = new GameLoop(this.jeu.getLoup(),this.loupVue);
-=======
 		this.loup = new Loup(null);
 		this.jeu = new Jeu(this.terrain);
 		this.terrainVue = new TerrainVue(this.tilePane, terrain);
 		this.terrainVue.afficherTerrainVue(21,21,12);
 		this.loupVue = new LoupVue(this.tableDeJeu, terrain);
 		this.tourVue = new TourVue(this.tableDeJeu, this.jeu);
->>>>>>> c1a4df185e0fc4296f5b48d3a3383acd606d6c94
 	}
 
 	@FXML
     void lancerPartie(ActionEvent event) {
-<<<<<<< HEAD
 //		System.out.println("X:" + loup.getX() + " Y:" + loup.getY());
 		//loupVue.afficherLoupVue(this.loup);
 		//this.loup.seDeplace();
 		this.gameloop.lancerAnimation();
-=======
 		this.loupVue.afficherLoupVue(this.loup);
 		this.loup.seDeplace();
->>>>>>> c1a4df185e0fc4296f5b48d3a3383acd606d6c94
     }
 	
 	@FXML
